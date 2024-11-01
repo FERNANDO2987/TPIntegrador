03ad6e635995870aceff7687558ad3e183c83e42
Fernando2987 <fernandopalacios51@gmail.com>
2024-11-01 16:04:08 -0300

    Revert "Reapply "Add: ModificarCliente""
    This reverts commit 528b04c5a43c776ecfbb7fafcdab81a0c7aeda47.

--- TPINT_GRUPO1_LAB4/src/datosImpl/ClienteDaoImpl.java
+++ TPINT_GRUPO1_LAB4/src/datosImpl/ClienteDaoImpl.java
@@ -1,10 +1,5 @@
 package datosImpl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
 import datos.ClienteDao;
 import entidad.Cliente;
 
@@ -58,93 +53,4 @@ public class ClienteDaoImpl implements ClienteDao{
 
 	    return estado;
 	}
-	
-	@Override
-	   public List<Cliente> obtenerClientes() {
-	        List<Cliente> clientes = new ArrayList<>();
-	        String query = "SELECT idCliente, dni, cuil, nombre, apellido, sexo, nacionalidad, fechaNacimiento, " +
-	                       "direccion, localidad, provincia, correoElectronico, telefono, usuario, contraseña " +
-	                       "FROM clientes";
-
-	        try {
-	            // Open connection
-	            cn = new Conexion();
-	    	    cn.Open();
-	            
-	           
-	            ResultSet rs = cn.query(query);
-
-	            while (rs != null && rs.next()) {
-	                Cliente cliente = new Cliente(
-	                        rs.getInt("idCliente"),
-	                        rs.getString("dni"),
-	                        rs.getString("cuil"),
-	                        rs.getString("nombre"),
-	                        rs.getString("apellido"),
-	                        rs.getString("sexo"),
-	                        rs.getString("nacionalidad"),
-	                        rs.getDate("fechaNacimiento").toLocalDate(),
-	                        rs.getString("direccion"),
-	                        rs.getString("localidad"),
-	                        rs.getString("provincia"),
-	                        rs.getString("correoElectronico"),
-	                        rs.getString("telefono"),
-	                        rs.getString("usuario"),
-	                        rs.getString("contraseña")
-	                );
-	                clientes.add(cliente);
-	            }
-
-	            System.out.println("Clientes cargados: " + clientes.size());
-
-	        } catch (SQLException e) {
-	            e.printStackTrace();
-	        } finally {
-	            // Close connection
-	            cn.close();
-	        }
-
-	        return clientes;
-	    }
-	
-	
-	@Override
-	public boolean modificarCliente(Cliente cliente) {
-	    boolean estado = true;
-
-	    cn = new Conexion();
-	    cn.Open();
-
-	    String query = "UPDATE clientes SET dni = '" + cliente.getDni() + "', "
-	            + "cuil = '" + cliente.getCuil() + "', "
-	            + "nombre = '" + cliente.getNombre() + "', "
-	            + "apellido = '" + cliente.getApellido() + "', "
-	            + "sexo = '" + cliente.getSexo() + "', "
-	            + "nacionalidad = '" + cliente.getNacionalidad() + "', "
-	            + "fechaNacimiento = '" + cliente.getFechaNacimiento() + "', "
-	            + "direccion = '" + cliente.getDireccion() + "', "
-	            + "localidad = '" + cliente.getLocalidad() + "', "
-	            + "provincia = '" + cliente.getProvincia() + "', "
-	            + "correoElectronico = '" + cliente.getCorreoElectronico() + "', "
-	            + "telefono = '" + cliente.getTelefono() + "', "
-	            + "usuario = '" + cliente.getUsuario() + "', "
-	            + "contraseña = '" + cliente.getContraseña() + "' "
-	            + "WHERE idCliente = " + cliente.getId();
-
-	    System.out.println(query);
-
-	    try {
-	        estado = cn.execute(query);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        estado = false;
-	    } finally {
-	        cn.close();
-	    }
-
-	    return estado;
-	}
-
-
-	
 }
