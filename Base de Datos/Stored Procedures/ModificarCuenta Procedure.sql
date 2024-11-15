b0c6d98a7edba279369f940d583a100302a2499a
Fernando2987 <fernandopalacios51@gmail.com>
2024-11-15 11:35:15 -0300

    Add: nuevo sp,tablas y un scrip de prueba

--- Base de Datos/Stored Procedures/ModificarCuenta Procedure.sql
+++
@@ -1,5 +0,0 @@
-DELIMITER $$
-CREATE PROCEDURE ModificarCuenta (IN nroCuentaIngresado BIGINT, IN idTipoCuentaIngresado int, IN borrado bit)
-BEGIN
-	UPDATE Cuentas SET id_tipo_cuenta = idTipoCuentaIngresado, deleted = borrado WHERE nro_cuenta = nroCuentaIngresado;
-END;
